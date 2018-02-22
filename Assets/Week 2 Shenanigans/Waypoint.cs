b155480c28e5d1d071d209517e573f8f02d53939
Acxell Leandro <33308802+JamaicanTea@users.noreply.github.com>
2018-02-22 15:54:24 +1100

    Week 2 Stuff done

---
+++ Assets/Week 2 Shenanigans/Waypoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour {
+	public WaypointHolder waypointHolder;
+
+	void Start ()
+	{
+		waypointHolder = GameObject.Find ("WaypointHolder").GetComponent<WaypointHolder> ();
+	}
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.name == "ColliderBottom") 
+		{
+			waypointHolder.UpdateScore();
+			waypointHolder.UpdateTime ();
+			Destroy (this.gameObject);
+		}
+	}
+		
+}
+
---
+++ Assets/Week 2 Shenanigans/WaypointHolder.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaypointHolder : MonoBehaviour {
+
+	public static float score;
+	public static float timer = 5f;
+	public Text scoreCounter;
+	public Text countdownTimer;
+
+	// Use this for initialization
+	public void UpdateScore ()
+	{
+		score += 100;
+		scoreCounter.text = score.ToString ();
+	}
+
+	public void UpdateTime ()
+	{
+		timer += 20;
+		countdownTimer.text = timer.ToString ();
+	}
+
+
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timer -= Time.deltaTime;
+		countdownTimer.text = timer.ToString ("##.##");
+	}
+		
+}
